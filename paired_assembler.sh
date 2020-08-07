5c9e83cbcebe8ce5a069c9880dc0ef607c2086a4
Rashedul Islam <rashedul.gen@gmail.com>
2020-08-06 23:26:11 -0700

    bioRxiv_version

--- paired_assembler.sh
+++ paired_assembler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $1 -> path of the paired_sra_list.txt
 # $2 -> path of the adapter file for PE
-# velvet, abyss and soapdenovo2: kmer = 21,63, 99 
+# velvet, abyss, ray and soapdenovo2: kmer = 21,63,99 
 # quast will not create output if contifs are <500bp
 
 mapfile -t sra_list < "${1}"
@@ -54,30 +54,6 @@ for sra in "${sra_list[@]}"; do
   mv trinity_out_dir.Trinity.fasta "trinity/${sra}/${sra}.fasta"
   quast_cp_zip_rm "trinity" "${sra}" "trinity/${sra}/${sra}.fasta"
 
-# abyss21
-  mkdir -p "abyss21/${sra}"
-  cd "abyss21/${sra}/"
-  abyss-pe name="${sra}" k=63 j=40 in="../../${pgz_1} ../../${pgz_2}"
-  rm *dot*
-  cd ../..
-  quast_cp_zip_rm "abyss21" "${sra}" "abyss21/${sra}/${sra}-scaffolds.fa"
-
-# abyss63
-  mkdir -p "abyss63/${sra}"
-  cd "abyss63/${sra}/"
-  abyss-pe name="${sra}" k=63 j=40 in="../../${pgz_1} ../../${pgz_2}"
-  rm *dot*
-  cd ../..
-  quast_cp_zip_rm "abyss63" "${sra}" "abyss63/${sra}/${sra}-scaffolds.fa"
-  
-# abyss99
-  mkdir -p "abyss99/${sra}"
-  cd "abyss99/${sra}/"
-  abyss-pe name="${sra}" k=99 j=40 in="../../${pgz_1} ../../${pgz_2}"
-  rm *dot*
-  cd ../..
-  quast_cp_zip_rm "abyss99" "${sra}" "abyss99/${sra}/${sra}-scaffolds.fa"  
-
 # spades
   mkdir -p "spades/${sra}"
   spades.py -1 "${pgz_1}" -2 "${pgz_2}" --rna -t 40 -o "spades/${sra}"
@@ -87,55 +63,55 @@ for sra in "${sra_list[@]}"; do
   mkdir -p "metaspades/${sra}"
   metaspades.py -1 "${pgz_1}" -2 "${pgz_2}" -t 40 -o "metaspades/${sra}"
   quast_cp_zip_rm "metaspades" "${sra}" "metaspades/${sra}/scaffolds.fasta"
-
-#  mkdir - p velvet
-#  velveth "velvet/${sra}" 21 -short -separate -fastq "${pgz_1}" "${pgz_2}"
-#  velvetg "velvet/${sra}" -read_trkg yes
-#  rm "velvet/${sra}/*Graph*"
-#  quast_cp_zip_rm "velvet" "${sra}" "velvet/${sra}/contigs.fa"
   
-#  velveth "metavelvet/${sra}" 21 -short -separate -fastq "${pgz_1}" "${pgz_2}"
-#  velvetg "metavelvet/${sra}" -read_trkg yes
-#  meta-velvetg "metavelvet/${sra}"
-#  rm "metavelvet/${sra}/*Graph*"
-#  quast_cp_zip_rm "metavelvet" "${sra}" "metavelvet/${sra}/contigs.fa"
+# abyss
+  for kmer in 21 63 99; do
+    mkdir -p "abyss${kmer}/${sra}"
+    cd "abyss${kmer}/${sra}/"
+    abyss-pe name="${sra}" k=$kmer j=40 in="../../${pgz_1} ../../${pgz_2}"
+    rm *dot*
+    cd ../..
+    quast_cp_zip_rm "abyss${kmer}" "${sra}" "abyss${kmer}/${sra}/${sra}-scaffolds.fa"
+  done 
+  
+# ray
+  for kmer in 21 63 99; do
+    mkdir -p "ray${kmer}"
+    cd "ray${kmer}"
+    gunzip ../"${pgz_1}" ../"${pgz_2}"
+    mpiexec -n 10 Ray -k $kmer -p  ../"trimmomatic/${sra}_1P.fastq" ../"trimmomatic/${sra}_2P.fastq" -o "${sra}"
+    rm -r "${sra}"/BiologicalAbundances/
+    cd ../
+    gzip "trimmomatic/${sra}_1P.fastq" "trimmomatic/${sra}_2P.fastq"
+    quast_cp_zip_rm "ray${kmer}" "${sra}" "ray${kmer}/${sra}/Scaffolds.fasta"
+  done
 
-# ray21
-  mkdir -p "ray21"
-  cd "ray21"
-  gunzip ../"${pgz_1}" ../"${pgz_2}"
-  mpiexec -n 10 Ray -k 21 -p  ../"trimmomatic/${sra}_1P.fastq" ../"trimmomatic/${sra}_2P.fastq" -o "${sra}"
-  rm -r "${sra}"/BiologicalAbundances/
-  cd ../
-  gzip "trimmomatic/${sra}_1P.fastq" "trimmomatic/${sra}_2P.fastq"
-  quast_cp_zip_rm "ray21" "${sra}" "ray21/${sra}/Scaffolds.fasta"
-
-# ray63
-  mkdir -p "ray63"
-  cd "ray63"
-  gunzip ../"${pgz_1}" ../"${pgz_2}"
-  mpiexec -n 10 Ray -k 63 -p  ../"trimmomatic/${sra}_1P.fastq" ../"trimmomatic/${sra}_2P.fastq" -o "${sra}"
-  rm -r "${sra}"/BiologicalAbundances/
-  cd ../
-  gzip "trimmomatic/${sra}_1P.fastq" "trimmomatic/${sra}_2P.fastq"
-  quast_cp_zip_rm "ray63" "${sra}" "ray63/${sra}/Scaffolds.fasta"
-
-# ray99
-  mkdir -p "ray99"
-  cd "ray99"
-  gunzip ../"${pgz_1}" ../"${pgz_2}"
-  mpiexec -n 10 Ray -k 99 -p  ../"trimmomatic/${sra}_1P.fastq" ../"trimmomatic/${sra}_2P.fastq" -o "${sra}"
-  rm -r "${sra}"/BiologicalAbundances/
-  cd ../
-  gzip "trimmomatic/${sra}_1P.fastq" "trimmomatic/${sra}_2P.fastq"
-  quast_cp_zip_rm "ray99" "${sra}" "ray99/${sra}/Scaffolds.fasta"
+#velvet and metavelvet
+  for kmer in 21 63 99; do
+    mkdir - p "velvet${kmer}"
+    velveth "velvet${kmer}/${sra}" $kmer -short -separate -fastq "${pgz_1}" "${pgz_2}"
+    velvetg "velvet${kmer}/${sra}" -read_trkg yes
+    rm -f velvet${kmer}/${sra}/*Graph*
+    quast_cp_zip_rm "velvet${kmer}" "${sra}" "velvet${kmer}/${sra}/contigs.fa"
+  
+    mkdir - p "metavelvet${kmer}"
+    velveth "metavelvet${kmer}/${sra}" $kmer -short -separate -fastq "${pgz_1}" "${pgz_2}"
+    velvetg "metavelvet${kmer}/${sra}" -read_trkg yes
+    meta-velvetg "metavelvet${kmer}/${sra}"
+    rm -f metavelvet${kmer}/${sra}/*Graph*
+    quast_cp_zip_rm "metavelvet${kmer}" "${sra}" "metavelvet${kmer}/${sra}/contigs.fa"
+  done
 
 
-  rm -rf "data/"
-  rm -rf "trimmomatic/"
+  rm -f "data/*"
+  rm -f "trimmomatic/*"
   
 done
 
-metaquast.py "output/*_PE.fasta" -o "output/meta_quast_out" -r MN908947.3.fasta -g Sars_cov_2.ASM985889v3.100.gff3 -t 40 --silent
-multiqc -f fastqc -o multiqc
-multiqc -f trimmed_fastqc -o trimmed_multiqc
+rm -rf megahit trinity spades metaspades abyss[0-9][0-9] ray[0-9][0-9] velvet[0-9][0-9] metavelvet[0-9][0-9]
+
+metaquast.py output/*.fasta -o "output/meta_quast_out" -r MN908947.3.fasta -g Sars_cov_2.ASM985889v3.100.gff3 -t 40 --silent
+
+# giving error: raise ImportError('A recent version of Python 3 is required.')
+#multiqc -f fastqc -o multiqc
+#multiqc -f trimmed_fastqc -o trimmed_multiqc
